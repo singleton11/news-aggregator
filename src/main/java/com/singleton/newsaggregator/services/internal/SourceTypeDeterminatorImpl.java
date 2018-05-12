612ef77568284db4cc837be91d4f048e3b5fdbfe
Anton Prokhorov <betrayer11@gmail.com>
2018-05-12 18:59:55 +0700

    Make initial `SourceTypeDeterminator` impl

---
+++ src/main/java/com/singleton/newsaggregator/services/internal/SourceTypeDeterminatorImpl.java
@@ -0,0 +1,38 @@
+package com.singleton.newsaggregator.services.internal;
+
+import com.singleton.newsaggregator.services.SourceTypeDeterminator;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import org.jsoup.Jsoup;
+import org.springframework.stereotype.Service;
+import org.xml.sax.InputSource;
+
+import java.io.StringReader;
+
+@Service
+public class SourceTypeDeterminatorImpl implements SourceTypeDeterminator {
+
+    public static boolean TYPE_HTML = true;
+    public static boolean TYPE_RSS = false;
+
+    private Object data = null;
+
+    @Override
+    public boolean determine(String data) {
+        try {
+            this.data = new SyndFeedInput().build(new InputSource(new StringReader(data)));
+            return TYPE_RSS;
+        } catch (FeedException e) {
+            this.data = Jsoup.parse(data);
+            return TYPE_HTML;
+        }
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
