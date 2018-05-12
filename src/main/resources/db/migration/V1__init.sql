7698825f7c191c742e327952ed24814d404a2b3d
Anton Prokhorov <betrayer11@gmail.com>
2018-05-13 02:30:02 +0700

    Fix initial migration according new entity config

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,13 +1,16 @@
-CREATE TABLE public.feed_entries
+CREATE SEQUENCE hibernate_sequence
+  START 1;
+
+CREATE TABLE public.feed_entry
 (
   id      SERIAL PRIMARY KEY       NOT NULL,
   created TIMESTAMP WITH TIME ZONE NOT NULL,
   title   VARCHAR(1000)            NOT NULL,
   link    VARCHAR(1000)            NOT NULL,
-  text    TEXT                     NOT NULL
+  body    TEXT                     NOT NULL
 );
 CREATE UNIQUE INDEX feed_entities_id_uindex
-  ON public.feed_entries (id);
+  ON public.feed_entry (id);
 
 CREATE UNIQUE INDEX feed_entities_link_uindex
-  ON public.feed_entries (link);
+  ON public.feed_entry (link);
