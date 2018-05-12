2500c513cbb5a3b485bf04220da57e7862bd7245
Anton Prokhorov <betrayer11@gmail.com>
2018-05-13 04:03:24 +0700

    Drop exposing unsafe methods

--- src/main/java/com/singleton/newsaggregator/repository/FeedEntryRepository.java
+++ src/main/java/com/singleton/newsaggregator/repository/FeedEntryRepository.java
@@ -2,6 +2,30 @@ package com.singleton.newsaggregator.repository;
 
 import com.singleton.newsaggregator.domain.FeedEntry;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface FeedEntryRepository extends CrudRepository<FeedEntry, Long> {
+    @Override
+    @RestResource(exported = false)
+    <S extends FeedEntry> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends FeedEntry> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(FeedEntry feedEntry);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends FeedEntry> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
