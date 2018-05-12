47ed14cfd2bce45541990fb91ae29a882b879583
Anton Prokhorov <betrayer11@gmail.com>
2018-05-13 01:55:45 +0700

    Fix initial migration

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,13 @@
-CREATE TABLE public.feed_entities
+CREATE TABLE public.feed_entries
 (
-  id      serial PRIMARY KEY       NOT NULL,
+  id      SERIAL PRIMARY KEY       NOT NULL,
   created TIMESTAMP WITH TIME ZONE NOT NULL,
-  title   varchar(1000)            NOT NULL,
+  title   VARCHAR(1000)            NOT NULL,
+  link    VARCHAR(1000)            NOT NULL,
   text    TEXT                     NOT NULL
 );
 CREATE UNIQUE INDEX feed_entities_id_uindex
-  ON public.feed_entities (id);
+  ON public.feed_entries (id);
+
+CREATE UNIQUE INDEX feed_entities_link_uindex
+  ON public.feed_entries (link);
