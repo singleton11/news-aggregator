e645bd53da005340a1fbed39f584b7ab46a3fdd9
Anton Prokhorov <betrayer11@gmail.com>
2018-05-13 23:49:23 +0700

    Add searching by title of entry

--- src/main/java/com/singleton/newsaggregator/repository/FeedEntryRepository.java
+++ src/main/java/com/singleton/newsaggregator/repository/FeedEntryRepository.java
@@ -1,10 +1,14 @@
 package com.singleton.newsaggregator.repository;
 
 import com.singleton.newsaggregator.domain.FeedEntry;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface FeedEntryRepository extends CrudRepository<FeedEntry, Long> {
+public interface FeedEntryRepository extends PagingAndSortingRepository<FeedEntry, Long> {
     @Override
     @RestResource(exported = false)
     <S extends FeedEntry> S save(S s);
@@ -28,4 +32,7 @@ public interface FeedEntryRepository extends CrudRepository<FeedEntry, Long> {
     @Override
     @RestResource(exported = false)
     void deleteAll();
+
+    @Query("SELECT e FROM FeedEntry e WHERE e.title LIKE %:title%")
+    Page<FeedEntry> searchByTitle(@Param("title") String title, Pageable page);
 }
